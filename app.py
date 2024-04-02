92c0c0fe9f47bc6a4e910aab58541480fd8fe3db
黃文輝 <antory040512@gmail.com>
2024-04-02 16:04:39 +0800

    初次提交

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,request, abort
+from linebot import LineBotApi,WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linenot.models import MessagwEvent,TextMessage,TextSendMseeade
+
+import bot_info
+app = Flask(__name__)
+line_bot_api = LineBotApi(bot_info.line_bot_token)
+handler = WebhookHandler(bot_info,line_bot_secert)
+
+@app.route('/callback',methods=['POST'])
+def callback();
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info('Reequest body:' + body)
+
+    try:
+        handler.handle(body,signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+@handler.add(MessagwEvent,message =TextMessage)
+def echo(event):
+    message = TextSendMseeade(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
+if __name__ == '__main__':
+    app.run()
